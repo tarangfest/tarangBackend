1d7847b7df58bc136adc2da07072fb1dc48750d2
Varun Singh <varun7singh10@gmail.com>
2023-10-07 04:22:05 +0530

    added registerEvent

--- routes/Events.js
+++ routes/Events.js
@@ -1,8 +1,10 @@
 const Router = require("express").Router();
-const { getEvent, getEvents } = require("../controllers/Events");
+const { getEvent, getEvents,registerEvent,removeEvent } = require("../controllers/Events");
 const { protectedRoute } = require("../middlewares/protectedRoute");
 
 Router.route("/").get(getEvents);
 Router.route("/:slug").get(getEvent);
+Router.route("/register").post(protectedRoute, registerEvent);
+Router.route("/remove").delete(protectedRoute, removeEvent);
 
 module.exports = Router;
